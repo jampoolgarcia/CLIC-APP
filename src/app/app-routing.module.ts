76d10ac3770bf7184c3e313a15e8bfad7bf796d7
jampool garcia <jampoolgarcia@gmail.com>
2023-02-07 01:46:05 -0300

    arreglado auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./layouts/layout/layout.module').then(m => m.LayoutModule),
+    path: 'auth',
+    loadChildren: () => import('@modules/auth/auth.module').then(m => m.AuthModule),
     //canActivate: [AuthGuard]
   },
-  {
-    path: 'auth',
-    loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
-  },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+  // },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'auth'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // components
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -38,6 +39,7 @@ import { AppComponent } from './app.component';
     provideAuth(() => getAuth()),
     BrowserAnimationsModule,
     FontAwesomeModule,
+    NgbModule,
   ],
   exports: [
     RouterModule,
